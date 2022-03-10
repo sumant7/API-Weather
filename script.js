ae06293540e97872b71deeb0db8f2edd8b2946f3
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-11 00:26:28 +0530

    Done

--- script.js
+++ script.js
@@ -21,5 +21,32 @@ function getResults(query){
 }
 
 function displayResults(weather){
-    console.log(weather)
+    let city = document.querySelector('.city')
+    city.innerText =  `${weather.name}, ${weather.sys.country}`
+
+    let now = new Date()
+    let date = document.querySelector('.date')
+    date.innerText = dateBuilder(now)
+
+    let temp = document.querySelector('.temp')
+    temp.innerHTML = `${Math.floor(weather.main.temp) -273}<span>°c</span>`
+
+    let weather_el= document.querySelector('.weather')
+    weather_el.innerText = weather.weather[0].main
+
+    let hilow = document.querySelector('.hi-low')
+    let hi = Math.round(weather.main.temp_min) -273
+    let low = Math.floor(weather.main.temp_max) -273
+    hilow.innerText= `${hi}°c / ${low}°c`
+}
+
+function dateBuilder(d){
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+    let day= days[d.getDay()]  //remember d.getDay() returns a number
+    let date = d.getDate()  
+    let month= months[d.getMonth()]  //remember d.getMonth() returns a number
+    let year = d.getFullYear();
+    return `${day}, ${date} ${month} ${year}`
 }
